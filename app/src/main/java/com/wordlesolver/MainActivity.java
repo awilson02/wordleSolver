f64d87b48b31a3a6a1e021c5d6fb3d59339d905d
awilson02 <88503300+awilson02@users.noreply.github.com>
2022-06-27 13:19:18 -0400

    Update MainActivity.java
    Uptated main Activity fixing bug where app crashed when at guess 6

--- app/src/main/java/com/wordlesolver/MainActivity.java
+++ app/src/main/java/com/wordlesolver/MainActivity.java
@@ -73,52 +73,7 @@ public class MainActivity extends AppCompatActivity {
             'k', 'l', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't',
             'u', 'v', 'w', 'x', 'y', 'z'));
-    public static ArrayList<Double> score = new ArrayList<Double>((Collection<? extends Double>) Arrays.asList(
-            0.09242295512473932, 0.025086892716459412, 0.031219587549239206, 0.037815710203135865, 0.10277284312968255,
-            0.017177724569398315, 0.02531860662701784, 0.027095079941299144, 0.05795937282768209, 0.004495249864833552,
-            0.023094153085656908, 0.05199660152931181, 0.030508998223526686, 0.045539507221750215, 0.06840194639684868,
-            0.031096006796941376, 0.0017301305321696147, 0.06416930563064803, 0.10271105275353364, 0.050853479570556887,
-            0.038742565845369586, 0.01070518266779949, 0.016019155016606165, 0.004448907082721866, 0.031914729280914496,
-            0.0067042558121572565));
 
-
-    public static ArrayList<ArrayList<Double>> positionScore = new ArrayList<ArrayList<Double>>
-            ((Collection<? extends ArrayList<Double>>) Arrays.asList(
-                    new ArrayList<Double>(Arrays.asList(0.12303577398863257, 0.37780006686726847, 0.2062855232363758, 0.17937144767636243, 0.11350718823136074)), //A
-                    new ArrayList<Double>(Arrays.asList(0.558841651263093, 0.04990757855822551, 0.20579174368453482, 0.14910659272951324, 0.036352433764633395)), //B
-                    new ArrayList<Double>(Arrays.asList(0.45544554455445546, 0.08712871287128712, 0.19405940594059407, 0.2004950495049505, 0.06287128712871287)), //C
-                    new ArrayList<Double>(Arrays.asList(0.27818627450980393, 0.03431372549019608, 0.15931372549019607, 0.19240196078431374, 0.33578431372549017)), //D
-                    new ArrayList<Double>(Arrays.asList(0.04542036396450594, 0.24454805233869756, 0.13235072943299744, 0.3493758459918785, 0.2283050082719206)), //E
-
-                    new ArrayList<Double>(Arrays.asList(0.5355535553555355, 0.021602160216021602, 0.15931593159315932, 0.20972097209720972, 0.0738073807380738)), //F
-                    new ArrayList<Double>(Arrays.asList(0.3888888888888889, 0.045787545787545784, 0.221001221001221, 0.2576312576312576, 0.08669108669108669)), //G
-                    new ArrayList<Double>(Arrays.asList(0.27837992013690815, 0.3103251568739304, 0.06845407872219053, 0.13405590416428978, 0.20878494010268112)), //H
-                    new ArrayList<Double>(Arrays.asList(0.04398826979472141, 0.3676352972540656, 0.27912556651559584, 0.23460410557184752, 0.07464676086376966)), //I
-                    new ArrayList<Double>(Arrays.asList(0.6941580756013745, 0.037800687285223365, 0.15807560137457044, 0.09965635738831616, 0.010309278350515464)),// J
-
-                    new ArrayList<Double>(Arrays.asList(0.2508361204013378, 0.06354515050167224, 0.17926421404682275, 0.33444816053511706, 0.17190635451505018)), //K
-                    new ArrayList<Double>(Arrays.asList(0.17087667161961367, 0.20683506686478453, 0.25200594353640415, 0.22912332838038632, 0.1411589895988113)), //L
-                    new ArrayList<Double>(Arrays.asList(0.35106382978723405, 0.09473150962512665, 0.25835866261398177, 0.20364741641337386, 0.09219858156028368)), // M
-                    new ArrayList<Double>(Arrays.asList(0.11024423337856173, 0.11702849389416553, 0.32632293080054275, 0.2666214382632293, 0.17978290366350066)), //N
-                    new ArrayList<Double>(Arrays.asList(0.05919566199728875, 0.4726615454134659, 0.22345232715770447, 0.1570266606416629, 0.087663804789878)), //O
-
-                    new ArrayList<Double>(Arrays.asList(0.425732737208147, 0.11326378539493294, 0.18032786885245902, 0.20765027322404372, 0.07302533532041729)), //P
-                    new ArrayList<Double>(Arrays.asList(0.6964285714285714, 0.13392857142857142, 0.11607142857142858, 0.017857142857142856, 0.03571428571428571)), //Q
-                    new ArrayList<Double>(Arrays.asList(0.1510115606936416, 0.22639691714836224, 0.28829479768786126, 0.17220616570327554, 0.16209055876685935)),//R
-                    new ArrayList<Double>(Arrays.asList(0.23465703971119134, 0.013989169675090252, 0.07987364620938628, 0.07746690734055355, 0.5940132370637786)), //S
-                    new ArrayList<Double>(Arrays.asList(0.2474164133738602, 0.07264437689969605, 0.18693009118541035, 0.2723404255319149, 0.22066869300911854)), //T
-
-                    new ArrayList<Double>(Arrays.asList(0.07535885167464115, 0.47248803827751196, 0.26555023923444976, 0.1598883572567783, 0.02671451355661882)), //U
-                    new ArrayList<Double>(Arrays.asList(0.3492063492063492, 0.07503607503607504, 0.3463203463203463, 0.22366522366522368, 0.005772005772005772)), //V
-                    new ArrayList<Double>(Arrays.asList(0.39633558341369335, 0.15718418514946964, 0.26133076181292186, 0.12343297974927676, 0.06171648987463838)), //W
-                    new ArrayList<Double>(Arrays.asList(0.05555555555555555, 0.19791666666666666, 0.4618055555555556, 0.041666666666666664, 0.24305555555555555)), //X
-                    new ArrayList<Double>(Arrays.asList(0.08760890609874153, 0.1292352371732817, 0.10309777347531461, 0.05227492739593417, 0.627783155856728)), //Y
-
-                    new ArrayList<Double>(Arrays.asList(0.24193548387096775, 0.06682027649769585, 0.3271889400921659, 0.2903225806451613, 0.07373271889400922)) //Z
-
-            )
-
-            );
 
 
     public static String wordGuess = "roate";
@@ -296,6 +251,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setButtonsText();
+<<<<<<< HEAD
+
+        final View anyView = findViewById(R.id.drawer_layout);
+        anyView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showInstructs();
+            }
+        }, 1);
+=======
     final View anyView = findViewById(R.id.drawer_layout);
         anyView.postDelayed(new Runnable() {
             @Override
@@ -304,13 +269,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 1);
 
+>>>>>>> b9159cf40fa1619e37da470dd3a650bd0a3c345b
     }
 
     @Override
     public void onStart() {
 
         super.onStart();
+<<<<<<< HEAD
+
+=======
    
+>>>>>>> b9159cf40fa1619e37da470dd3a650bd0a3c345b
 
     }
 
@@ -332,6 +302,7 @@ public class MainActivity extends AppCompatActivity {
 
         boolean changed = false;
         listCleaner();
+        Probabilty.updateProb(usedChar);
 
         for (int i = 0; i < words.size(); i++) {
             if (!changed) {
@@ -465,7 +436,7 @@ public class MainActivity extends AppCompatActivity {
 
         //letter value
         for (int i = 0; i < string.length(); i++) {
-            value += score.get(letters.indexOf(string.charAt(i)));
+            value += Probabilty.score.get(letters.indexOf(string.charAt(i)));
         }
 
         //letter position value
@@ -473,7 +444,7 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < string.length(); i++) {
 
                 if (yellowWord.contains(string.charAt(i))) {
-                    value += positionScore.get(letters.indexOf(string.charAt(i))).get(i);
+                    value += Probabilty.positionScore.get(letters.indexOf(string.charAt(i))).get(i);
                 }
             }
         }
@@ -619,7 +590,7 @@ public class MainActivity extends AppCompatActivity {
                     ((EditText) findViewById(R.id.userText3)).setText("");
                     ((EditText) findViewById(R.id.userText4)).setText("");
                     ((EditText) findViewById(R.id.userText5)).setText("");
-                    ((Button) findViewById(R.id.SubmitButton)).setText("Submit Results");
+                    ((Button) findViewById(R.id.SubmitButton)).setText("Submit ");
                     ((Button) findViewById(R.id.submitGuess)).setText("make a guess");
                     ((Button) findViewById(R.id.SubmitButton)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_green, null));
                     ((Button) findViewById(R.id.submitGuess)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_enter_grey, null));
@@ -995,12 +966,12 @@ public class MainActivity extends AppCompatActivity {
                                 setAdapter();
                             }
                             else
-                                {
-                                    setAdapter();
-                                    setButtonsText();
-                                    row.setVisibility(View.VISIBLE);
-                                    rowPrev.setBackground(Drawable.createFromPath("@null"));
-                                }
+                            {
+                                setAdapter();
+                                setButtonsText();
+                                row.setVisibility(View.VISIBLE);
+                                rowPrev.setBackground(Drawable.createFromPath("@null"));
+                            }
                         }
                     } else {
                         rowPrev.setBackground(Drawable.createFromPath("@null"));
@@ -1011,6 +982,18 @@ public class MainActivity extends AppCompatActivity {
 
 
                 }
+<<<<<<< HEAD
+
+                for(int x = 0; x < word.size(); x++)
+                {
+                    if(guess !=6) {
+                        if (word.get(x) != '-') {
+                            tileButtons.get(guess).get(x).setBackgroundColor(Color.parseColor("#458802"));
+                            tileButtons.get(guess).get(x).setEnabled(false);
+                        }
+                    }
+                }
+=======
                 //check if letter is already know if so make it green and cannot change 
                 for(int x = 0; x < word.size(); x++)
                 {
@@ -1021,6 +1004,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 
+>>>>>>> b9159cf40fa1619e37da470dd3a650bd0a3c345b
                 break;
             case R.id.resetButton:
 
@@ -1045,51 +1029,51 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                    mLastClickTime = SystemClock.elapsedRealtime();
-                    if (!userSubmit) {
+                mLastClickTime = SystemClock.elapsedRealtime();
+                if (!userSubmit) {
 
-                        for (int x = 0; x < 5; x++) {
-                            tileButtons.get(guess).get(x).setEnabled(false);
-                        }
-                        LinearLayout text = findViewById(R.id.userGuess);
-                        text.setVisibility(View.VISIBLE);
-
-                        ((Button) findViewById(R.id.submitGuess)).setText("Guess");
-                        ((Button) findViewById(R.id.SubmitButton)).setText("Cancel");
-                        ((Button) findViewById(R.id.SubmitButton)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_red, null));
-                        ((Button) findViewById(R.id.submitGuess)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_green, null));
-
-
-                        if (findViewById(R.id.userText1).requestFocus()) {
-                            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-                            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                            imm.showSoftInput(findViewById(R.id.userText1), InputMethodManager.SHOW_IMPLICIT);
-                        }
-
-                        userSubmit = true;
-                    } else {
-                        for (int x = 0; x < 5; x++) {
-                            tileButtons.get(guess).get(x).setEnabled(true);
-                        }
-                        getUserText();
-                        setButtonsText();
-
-                        LinearLayout text = findViewById(R.id.userGuess);
-                        text.setVisibility(View.GONE);
-
-                        ((EditText) findViewById(R.id.userText1)).setText("");
-                        ((EditText) findViewById(R.id.userText2)).setText("");
-                        ((EditText) findViewById(R.id.userText3)).setText("");
-                        ((EditText) findViewById(R.id.userText4)).setText("");
-                        ((EditText) findViewById(R.id.userText5)).setText("");
-
-
-                        ((Button) findViewById(R.id.SubmitButton)).setText("Submit Results");
-                        ((Button) findViewById(R.id.submitGuess)).setText("make a guess");
-                        ((Button) findViewById(R.id.SubmitButton)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_green, null));
-                        ((Button) findViewById(R.id.submitGuess)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_enter_grey, null));
-                        userSubmit = false;
+                    for (int x = 0; x < 5; x++) {
+                        tileButtons.get(guess).get(x).setEnabled(false);
                     }
+                    LinearLayout text = findViewById(R.id.userGuess);
+                    text.setVisibility(View.VISIBLE);
+
+                    ((Button) findViewById(R.id.submitGuess)).setText("Guess");
+                    ((Button) findViewById(R.id.SubmitButton)).setText("Cancel");
+                    ((Button) findViewById(R.id.SubmitButton)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_red, null));
+                    ((Button) findViewById(R.id.submitGuess)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_green, null));
+
+
+                    if (findViewById(R.id.userText1).requestFocus()) {
+                        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.showSoftInput(findViewById(R.id.userText1), InputMethodManager.SHOW_IMPLICIT);
+                    }
+
+                    userSubmit = true;
+                } else {
+                    for (int x = 0; x < 5; x++) {
+                        tileButtons.get(guess).get(x).setEnabled(true);
+                    }
+                    getUserText();
+                    setButtonsText();
+
+                    LinearLayout text = findViewById(R.id.userGuess);
+                    text.setVisibility(View.GONE);
+
+                    ((EditText) findViewById(R.id.userText1)).setText("");
+                    ((EditText) findViewById(R.id.userText2)).setText("");
+                    ((EditText) findViewById(R.id.userText3)).setText("");
+                    ((EditText) findViewById(R.id.userText4)).setText("");
+                    ((EditText) findViewById(R.id.userText5)).setText("");
+
+
+                    ((Button) findViewById(R.id.SubmitButton)).setText("Submit");
+                    ((Button) findViewById(R.id.submitGuess)).setText("make a guess");
+                    ((Button) findViewById(R.id.SubmitButton)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_green, null));
+                    ((Button) findViewById(R.id.submitGuess)).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corner_enter_grey, null));
+                    userSubmit = false;
+                }
 
                 break;
             case R.id.drawerButton:
@@ -1159,13 +1143,13 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
-           return true;
+            return true;
         }
         else
-            {
-                Toast.makeText(this, "Please fill all spaces in guess", Toast.LENGTH_LONG).show();
-                return false;
-            }
+        {
+            Toast.makeText(this, "Please fill all spaces in guess", Toast.LENGTH_LONG).show();
+            return false;
+        }
     }
 
 
